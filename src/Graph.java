72a8c9ebda11e4d8676880d43a6832603e4f2b2e
alessioditomasso <alessio.ditomasso@student.vinci.be>
2022-03-09 11:28:14 +0100

    add - class graph constructeur + method

---
+++ src/Graph.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class Graph {
+
+  public Graph(File aeroport, File vols){
+
+  }
+
+
+  public String calculerItineraireMinimisantNombreVol(String iataSource, String iataDestination){
+    return "";
+  }
+
+  public String calculerItineraireMiniminantDistance(String iataSource, String iataDestination){
+    return "";
+  }
+}
