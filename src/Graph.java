b33242df908223ee8504e437594e9944323c59e6
bdeTheux <boris.detheux@student.vinci.be>
2022-03-16 12:25:44 +0100

    Add - ListeAdj

--- src/Graph.java
+++ src/Graph.java
@@ -1,11 +1,20 @@
 import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Graph {
 
+  private Map<Aeroport, Set<Vol>>outputFlights;
+  //key: iata, value: aeroport
+  private Map<String, Aeroport> iataToAeroport;
+
   public Graph(File aeroport, File vols) throws IOException {
-
+    outputFlights = new HashMap<Aeroport,Set<Vol>>();
+    iataToAeroport = new HashMap<String, Aeroport>();
     parsing(aeroport);
-
+    parsing(vols);
 
   }
 
@@ -25,15 +34,21 @@ public class Graph {
       String[] ligneCoupe = ligne.split(",");
       if(ligneCoupe.length > 3){
         Aeroport aeroport = new Aeroport(ligneCoupe[0], ligneCoupe[1], ligneCoupe[2], ligneCoupe[3], Double.parseDouble(ligneCoupe[4]), Double.parseDouble(ligneCoupe[5]));
-        System.out.println(ligneCoupe[0]);
+        outputFlights.put(aeroport, new HashSet<Vol>());
+        iataToAeroport.put(aeroport.getIata(), aeroport);
+      }else{
+        Vol vol = new Vol(ligneCoupe[0],ligneCoupe[1], ligneCoupe[2]);
+        outputFlights.get(iataToAeroport.get(vol.getSource())).add(vol);
+      }
+    }
+  }
+}
+
+/*
+System.out.println(ligneCoupe[0]);
         System.out.println(ligneCoupe[1]);
         System.out.println(ligneCoupe[2]);
         System.out.println(ligneCoupe[3]);
         System.out.println(ligneCoupe[4]);
         System.out.println(ligneCoupe[5]);
-      }else{
-        Vols vols = new Vols(ligneCoupe[0],ligneCoupe[1], ligneCoupe[2]);
-      }
-    }
-  }
-}
+ */
--- src/Vol.java
+++ src/Vol.java
@@ -1,10 +1,10 @@
-public class Vols {
+public class Vol {
 
   private final String airline;
   private final String source;
   private final String destination;
 
-  public Vols(String airline, String source, String destination) {
+  public Vol(String airline, String source, String destination) {
     this.source = source;
     this.destination = destination;
     this.airline = airline;
